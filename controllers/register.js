e8451347b1f4cdb208add9ccfe2e6ee98aa05497
uglyCode <appostnikov@gmail.com>
2019-04-22 22:31:13 +0300

    preparing for profile managment

--- controllers/register.js
+++ controllers/register.js
@@ -1,7 +1,7 @@
 const handleRegister = (req, res, pg, bcrypt) => {
-    const {email, name, password} = req.body;
+    const {inn, kpp, contact, address, email, name, password} = req.body;
 
-    if(!name || !email || !password){
+    if(!inn || !email || !password){
         return res.status('400').json('bad request');
     }
 
@@ -10,15 +10,20 @@ const handleRegister = (req, res, pg, bcrypt) => {
     pg.transaction(trx => {
         trx.insert({
             hash: hash,
-            email: email
+            inn: inn
         })
             .into('login')
-            .returning('email')
-            .then(loginEmail => {
+            .returning('inn')
+            .then(loginInn => {
                 return trx('users')
                     .returning('*')
                     .insert({
-                        email: loginEmail[0],
+                        inn: loginInn[0],
+                        kpp: kpp,
+                        contact: contact,
+                        address: address,
+                        email: email,
+                        phone: phone,
                         name: name,
                         joined: new Date()})
                     .then(userData => res.json(userData[0]))
