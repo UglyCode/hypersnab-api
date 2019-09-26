64213c45d3b9e409d6d3feddc007b068da23611c
uglyCode <appostnikov@gmail.com>
2019-09-26 22:20:47 +0300

    pg seed file data updated

--- postgres/tables/folders_create.sql
+++ postgres/tables/folders_create.sql
@@ -3,6 +3,7 @@ BEGIN TRANSACTION;
 CREATE TABLE folders(
     code INTEGER PRIMARY KEY,
     name VARCHAR(128) NOT NULL,
+    level INTEGER,
     parent INTEGER,
     image VARCHAR(256),
     updated TIMESTAMP
