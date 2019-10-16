75f497a6dbb133f1a36533a1a626a95b389a8437
uglyCode <appostnikov@gmail.com>
2019-10-16 22:00:34 +0300

    img

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 -- Seed data with a fake user for testing
 insert into users (inn, kpp, name, email, phone, contact, address, joined)
-    values (7704409586, 770000114, 'a', 'a@a.com', 79995555555, 'mr.Robot', 'at the middle of nowhere','2019-01-01');
+    values (7704409586, 770000114, 'a', 'anton@an.com', 79995555555, 'mr.Robot', 'at the middle of nowhere','2019-01-01');
 insert into login (hash, inn) values ('$2a$10$WAK21U0LWl7C//jJ.DOB2uPP1DJQh7KUDgasdyQeGzkop2Pzl8W7u', 7704409586);
 
 --goods initial data for testing
