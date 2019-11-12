d13c1b1c882441854838eba0f291764d06317197
uglyCode <appostnikov@gmail.com>
2019-11-12 22:38:11 +0300

    attributes table

--- postgres/tables/6_foldersattributes.sql
+++ postgres/tables/6_foldersattributes.sql
@@ -4,8 +4,8 @@
 
 CREATE TABLE public.folders_attributes (
 	folder varchar(16) NOT NULL,
-	"attribute" int4 NOT NULL,
-	"name" varchar(32) NULL,
+	"attribute_code" int4 NOT NULL,
+	"attribute_name" varchar(32) NULL,
 	CONSTRAINT folders_attributes_pk PRIMARY KEY (folder, attribute),
 	CONSTRAINT folders_attributes_fk FOREIGN KEY (folder) REFERENCES folders(code),
 	CONSTRAINT folders_attributes_fk_1 FOREIGN KEY (attribute) REFERENCES attributes(code)
