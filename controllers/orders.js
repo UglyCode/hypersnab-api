259d88fefa9abb316408c934c2542ef946d88329
uglyCode <appostnikov@gmail.com>
2020-05-24 21:58:32 +0300

    delivery date fixed at orders

--- controllers/orders.js
+++ controllers/orders.js
@@ -46,7 +46,7 @@ const handleOrderPost = (req, res) => {
 
     client
         .query(`INSERT INTO public.orders (client,status,comm, delivery, delivery_address, delivery_date) 
-            VALUES (${body.inn},'Новый','${body.comment}', ${body.delivery}, '${body.delivery_address}','${body.delivery_date}) RETURNING id;`)
+            VALUES (${body.inn},'Новый','${body.comment}', ${body.delivery}, '${body.delivery_address}','${body.delivery_date}') RETURNING id;`)
         .then( result => {
             return client.query('INSERT INTO public.ordered_goods ("order",good,amount,price) VALUES ' +
                 body.orderedGoods.reduce((accum, elem, i, arr) => {
