5ae23e490c83e47ceb32996ed0ab185b47beb2b1
uglyCode <appostnikov@gmail.com>
2020-06-16 11:04:50 +0300

    cloudinary added to project cooly

--- controllers/cloudinary.js
+++ controllers/cloudinary.js
@@ -4,10 +4,9 @@ const cloudinary = require('cloudinary');
 handlePricePost = (req, res) => {
     stream = cloudinary.uploader.upload_stream(function(result) {
         console.log(result);
-        res.send('Done:<br/> <img src="' + result.url + '"/><br/>' +
-            cloudinary.image(result.public_id, { format: "png", width: 100, height: 130, crop: "fill" }));
-    }, { public_id: req.body.title } );
-    fs.createReadStream(req.files.image.path, {encoding: 'binary'}).on('data', stream.write).on('end', stream.end);
+        res.send('Ok');
+    }, { public_id: 'price.xls' });
+    fs.createReadStream(req, {encoding: 'binary'}).on('data', stream.write).on('end', stream.end);
 };
 
 module.exports = {handlePricePost};
