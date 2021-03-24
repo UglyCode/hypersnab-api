0d232d00d24467b8fbe8662be7aaf66c1d10f822
uglyCode <appostnikov@gmail.com>
2021-03-24 16:19:57 +0300

    ssl pg

--- controllers/goods.js
+++ controllers/goods.js
@@ -1,7 +1,4 @@
-const {Client} = require('pg');
-const connectionString = process.env.DATABASE_URL;
 const { Client } = require('pg');
-
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: {
