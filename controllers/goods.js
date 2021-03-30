0ddbd53429ece4f207a39aa686a78c7f9990f2b2
uglyCode <appostnikov@gmail.com>
2021-03-30 08:02:39 +0300

    mose ssl in link

--- controllers/goods.js
+++ controllers/goods.js
@@ -1,4 +1,4 @@
-console.log('connecting ....');
+console.log('connecting .... testing heroku-20');
 const { Client } = require('pg');
 const client = new Client({
     connectionString: process.env.DATABASE_URL + '?sslmode=require',
