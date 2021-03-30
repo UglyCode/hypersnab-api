f66cf6d11e7b9e697e52b147da3dd9adaf91531d
uglyCode <appostnikov@gmail.com>
2021-03-30 07:40:22 +0300

    mose ssl in link

--- controllers/goods.js
+++ controllers/goods.js
@@ -1,7 +1,7 @@
 console.log('connecting ....');
 const { Client } = require('pg');
 const client = new Client({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL + '?sslmode=require',
     ssl: {
         rejectUnauthorized: false
     },
